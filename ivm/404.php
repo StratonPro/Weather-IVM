e9829a6b8337efc223de08c7cfbe7949899577db
Straton.Pro <63187237+StratonPro@users.noreply.github.com>
2023-04-14 20:20:47 +0300

    404.php
    Signed-off-by: Straton.Pro <63187237+StratonPro@users.noreply.github.com>

---
+++ ivm/404.php
@@ -0,0 +1,70 @@
+<?php include("html.php"); ?>
+
+<!-- 
+//////////////////////////////////////////////////////
+
+Straton plc.
+straton.pro
+
+Weather Forecasting Agency
+
+//////////////////////////////////////////////////////
+-->
+ 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
+    <!-- Meta description, aim for about 150 characters-->
+    <meta http-equiv='content-language' content='en-gb'>
+    <meta http-equiv="cleartype" content="on" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="HandheldFriendly" content="true" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="author" content="Straton®"/>
+    <meta name="generator" content="straton.pro">
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+    
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+    
+		<title>straton.pro | 404</title>
+        
+        <!-- css  -->
+        <link rel="stylesheet" href="css/404.css" crossorigin="anonymous"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+        <link rel="manifest" href="favicon/site.webmanifest">
+        <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
+</head>
+
+<body>
+
+    <!-- Load page -->
+        <div title="404">404</div>
+    <!-- End load page -->
+
+
+</body>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JWDNGENTXF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JWDNGENTXF');
+</script>
+
+</html>
